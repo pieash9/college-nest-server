83d79799abdeb83f4e3207918aa2f6c433cd9c21
Md. pieash ahmed <81944951+pieash9@users.noreply.github.com>
2023-07-23 22:34:31 +0600

    added reviews

--- index.js
+++ index.js
@@ -28,16 +28,38 @@ async function run() {
 
     const collegeCollection = client.db("collegeNestDB").collection("colleges");
     const userCollection = client.db("collegeNestDB").collection("users");
+    const appliedCollegeCollection = client
+      .db("collegeNestDB")
+      .collection("appliedColleges");
+    const feedbackCollection = client
+      .db("collegeNestDB")
+      .collection("feedbacks");
 
     //post user  to server
     app.post("/users", async (req, res) => {
       const userInfo = req.body;
       if (await userCollection.findOne({ email: userInfo.email })) {
-        return;
+        return res.send({ login: true });
       }
       const result = await userCollection.insertOne(userInfo);
       res.send(result);
     });
+
+    //post applied college
+    app.post("/appliedCollege", async (req, res) => {
+      const applicationData = req.body;
+      const result = await appliedCollegeCollection.insertOne(applicationData);
+      res.send(result);
+    });
+
+    //get applied College data for a user
+    app.get("/appliedCollege/:userEmail", async (req, res) => {
+      const email = req.params.userEmail;
+      const query = { candidateEmail: email };
+      const result = await appliedCollegeCollection.find(query).toArray();
+      res.send(result);
+    });
+
     //get all college
     app.get("/colleges", async (req, res) => {
       const result = await collegeCollection.find().toArray();
@@ -49,6 +71,22 @@ async function run() {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await collegeCollection.findOne(query);
+      res.send(result);
+    });
+
+    //  post feedback data
+    app.post("/feedback", async (req, res) => {
+      const feedbackInfo = req.body;
+      const result = await feedbackCollection.insertOne(feedbackInfo);
+      res.send(result);
+    });
+
+    //get all feedback
+    app.get("/feedback", async (req, res) => {
+      const result = await feedbackCollection
+        .find()
+        .sort({ createdAt: -1 })
+        .toArray();
       res.send(result);
     });
 
